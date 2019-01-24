0131be392deda380cba78f501d4366c5410dd2af
Kamila Wickramarachchi <lakith7@berkeley.edu>
2019-01-24 12:54:46 -0800

    completed Lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Kamila Wickramarachchi
  */
 public class LeapYear {
 
@@ -29,5 +29,16 @@ public class LeapYear {
             }
         }
     }
-}
 
+    /** Determines whether the inputted year is a leap year or not. */
+    public static boolean isLeapYear(int year) {
+      if (year % 400 == 0) {
+        return true;
+      } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+          }
+       else {
+        return false;
+      }
+    }
+}
