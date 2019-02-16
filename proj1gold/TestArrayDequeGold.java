74627b28a4ff307c9da49bbfe31a470173348a1b
Kamila Wickramarachchi <lakith7@berkeley.edu>
2019-02-15 17:40:18 -0800

    Finished proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,25 +6,38 @@ public class TestArrayDequeGold {
     @Test
     public void testingArray() {
         StudentArrayDeque<Integer> tester = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> actual = new ArrayDequeSolution<>();
+        ArrayDequeSolution<Integer> actuality = new ArrayDequeSolution<>();
         int tracker = 0;
         int m = 0;
         int n = 0;
-        while (tracker < 5) {
-            n = StdRandom.uniform(-9999999, 9999999);
-            tester.addFirst(n);
-            actual.addFirst(n);
-            tester.addLast(n*2);
-            actual.addLast(n*2);
+        while (tracker < 10) {
+            m = StdRandom.uniform(100);
+            tester.addFirst(m);
+            actuality.addFirst(m);
             tracker += 1;
         }
-        while (m < 2) {
-            Integer holding = actual.removeFirst();
-            Integer holdingEnd = actual.removeLast();
-            assertEquals("\naddFirst(" + holding + ")\nremoveFirst()" , tester.removeFirst(), holding);
-            assertEquals("\naddLast(" + holdingEnd + ")\nremoveLast()" , tester.removeLast(), holdingEnd);
-            m += 1;
+
+        while (tracker > 0) {
+            Integer actual = actuality.removeFirst();
+            Integer test = tester.removeFirst();
+            assertEquals("\naddFirst(" + actual + ")\nremoveFirst()", actual, test);
+            tracker -= 1;
         }
+
+        while (tracker < 10) {
+            m = StdRandom.uniform(100);
+            tester.addLast(m);
+            actuality.addLast(m);
+            tracker += 1;
+        }
+
+        while (tracker > 0) {
+            Integer actual = actuality.removeLast();
+            Integer test = tester.removeLast();
+            assertEquals("\naddLast(" + actual + ")\nremoveLast()", actual, test);
+            tracker -= 1;
+        }
+
 
     }
 
