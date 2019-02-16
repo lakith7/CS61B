ddbfd7d922c79616bd8a68e59679805438f5bf36
Kamila Wickramarachchi <lakith7@berkeley.edu>
2019-02-16 15:42:02 -0800

    Finished proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,8 +1,129 @@
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class TestArrayDequeGold {
 
+
+    @Test
+    public void testUpdateArray() {
+        StudentArrayDeque<Integer> test = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> actual = new ArrayDequeSolution<>();
+        int repeater = 0;
+        int correct = 0;
+        int wrong = 0;
+        String message = "";
+        while (repeater < 500) {
+            int checker = StdRandom.uniform(1, 3);
+            if (checker == 1) {
+                int holder = StdRandom.uniform(1000);
+                test.addFirst(holder);
+                actual.addFirst(holder);
+            }
+            if (checker == 2) {
+                int holder = StdRandom.uniform(1000);
+                test.addLast(holder);
+                actual.addLast(holder);
+            }
+            repeater += 1;
+        }
+
+
+        while (repeater > 50) {
+            int checker = StdRandom.uniform(1, 3);
+            if (checker == 1) {
+                correct = actual.removeFirst();
+                wrong = test.removeFirst();
+                message = stringer(correct, actual, false);
+                assertEquals(message, correct, wrong);
+            }
+
+            if (checker == 2) {
+                correct = actual.removeLast();
+                wrong = test.removeLast();
+                message = stringer(correct, actual, true);
+                assertEquals(message, correct, wrong);
+            }
+            repeater -= 1;
+        }
+
+    }
+
+
+    public String stringer(int a, ArrayDequeSolution<Integer> b, boolean c) {
+        String messaging = "";
+        int index = 0;
+        while (index < b.size()) {
+            messaging += "\naddLast(" + b.removeFirst() + ")";
+            index += 1;
+        }
+        messaging += "\naddLast(" + a + ")";
+        if (c) {
+            messaging += "\nremoveLast()";
+        }
+        if (!c) {
+            messaging += "\nremoveFirst()";
+        }
+        return messaging;
+    }
+
+
+    /*
+    @Test
+    public void testUpdateArray() {
+        StudentArrayDeque<Integer> test = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> actual = new ArrayDequeSolution<>();
+        int repeater = 0;
+        int correct = 0;
+        int wrong = 0;
+        while (repeater < 10000) {
+            int checker = StdRandom.uniform(1, 4);
+            if (checker == 1) {
+                int holder = StdRandom.uniform(1000);
+                test.addFirst(holder);
+                actual.addFirst(holder);
+            }
+            if (checker == 2) {
+                int holder = StdRandom.uniform(1000);
+                test.addLast(holder);
+                actual.addLast(holder);
+            }
+            if (checker == 3) {
+                correct = actual.removeFirst();
+                wrong = test.removeFirst();
+                assertEquals(stringer(correct, actual, true), correct, wrong);
+            }
+
+            if (checker == 4) {
+                assertEquals(test.removeLast(), actual.removeLast());
+            }
+
+            repeater += 1;
+        }
+
+    }
+
+
+
+    public String stringer(int a, ArrayDequeSolution<Integer> b, boolean c) {
+        String messaging = "";
+        while (b != null) {
+            messaging += "\naddLast(" + b.removeFirst() + ")";
+        }
+        messaging += "\naddLast(" + a + ")";
+        if (c) {
+            messaging += "\nremoveLast()";
+        }
+        if (!c) {
+            messaging += "\nremoveFirst()";
+        }
+        return messaging;
+    }
+
+    */
+
+
+    /*
     @Test
     public void testingArray() {
         StudentArrayDeque<Integer> tester = new StudentArrayDeque<>();
@@ -26,33 +147,21 @@ public class TestArrayDequeGold {
             Integer actual = actuality.removeLast();
             Integer test = tester.removeLast();
             int index = 0;
-            Integer h = 0;
             String messaging = "";
+
             while (tracking[index] != actual) {
                 messaging += "\naddLast(" + tracking[index] + ")";
-                h = tracking[index];
                 index += 1;
             }
             messaging += "\naddLast(" + actual + ")";
             messaging += "\nremoveLast()";
-
             assertEquals(messaging, actual, test);
             tracker -= 1;
         }
 
 
     }
+    */
 
-    public String stringer(int a, int[] b) {
-        int index = 0;
-        String messaging = "";
-        while (b[index] != a) {
-            messaging += "\naddLast(" + b[index] + ")";
-            index += 1;
-        }
-        messaging += "\nremoveLast()";
-        return messaging;
-
-    }
 
 }
