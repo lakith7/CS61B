747c5d6b21b39948c29e9b0d895daf30a355694b
Kamila Wickramarachchi <lakith7@berkeley.edu>
2019-03-11 14:54:21 -0700

    Finished hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,8 +1,11 @@
 package hw3.hash;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+
+import static org.junit.Assert.assertTrue;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
@@ -17,27 +20,45 @@ public class OomageTestUtility {
 
         double q = oomages.size();
 
-        Map<Integer, Integer> holder = new HashMap<>();
+        Map<Integer, Integer> holder = new HashMap<>(oomages.size());
         for (int n = 0; n < oomages.size(); n += 1) {
-            int bucketNum = (oomages.get(0).hashCode() & 0x7FFFFFFF) % M;
+            int bucketNum = ((oomages.get(n).hashCode() & 0x7FFFFFFF) % M);
             if (holder.containsKey(bucketNum)) {
-                holder.replace(bucketNum, (int) holder.get(bucketNum) + 1);
+                holder.replace(bucketNum, holder.get(bucketNum) + 1);
             } else {
                 holder.put(bucketNum, 1);
             }
         }
 
-        Object[] tester = holder.values().toArray();
+        ArrayList<Integer> tester = new ArrayList<>();
+        for (Integer each: holder.keySet()) {
+            tester.add(holder.get(each));
+        }
+
         double upperLimit = q/50;
         double lowerLimit = q/2.5;
-        for (int i = 0; i < tester.length; i += 1) {
-            if ((int) tester[i] > upperLimit) {
+        for (int i = 0; i < tester.size(); i += 1) {
+            if (tester.get(i) == 0) {
+                return true;
+            }
+            if (tester.get(i) < upperLimit) {
                 return false;
-            } else if ((int) tester[i] < lowerLimit) {
+            } else if (tester.get(i) > lowerLimit) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+        List<Oomage> oomages = new ArrayList<>();
+        int N = 10000;
+
+        for (int i = 0; i < N; i += 1) {
+            oomages.add(ComplexOomage.randomComplexOomage());
+        }
+
+        System.out.println(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -35,7 +35,7 @@ public class TestComplexOomage {
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
+
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
@@ -43,7 +43,7 @@ public class TestComplexOomage {
         // Your code here.
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
